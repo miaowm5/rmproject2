e3ec6a5ec3b96ff14ffa7aca29b34e9b4623d245
miaowm5 <miaowm5@gmail.com>
2020-07-23 21:41:14 +0800

    add workflow

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 RPG 制作大师授权素材推广计划 2.0
 
-![Build Frontend Page](https://github.com/miaowm5/rmproject2/workflows/Build%20Frontend%20Page/badge.svg?branch=frontend)
+![Frontend Page](https://github.com/miaowm5/rmproject2/workflows/Build%20Frontend%20Page/badge.svg?branch=frontend)
+![Backend API](https://github.com/miaowm5/rmproject2/workflows/Build%20Backend%20API/badge.svg?branch=master)
--- deploy/rule.js
+++ deploy/rule.js
@@ -7,7 +7,8 @@ const rulePath = path.join(mainPath, 'rule')
 
 const main = async ()=>{
   const result = {}
-  fse.readdirSync(rulePath).forEach((filename)=>{
+  const fileList = await fse.readdir(rulePath)
+  fileList.forEach((filename)=>{
     if (!filename.endsWith('.json')) return
     const id = filename.slice(0, filename.length - 5) - 0
     if (!id) return
@@ -15,9 +16,14 @@ const main = async ()=>{
     config.id = id
     result[id] = config
   })
-  const targetPath = path.join(mainPath, 'build')
-  fse.ensureDirSync(targetPath)
-  fse.writeFileSync(path.join(targetPath, 'rule.json'), JSON.stringify(result))
+
+  const targetPath = path.join(mainPath, 'build/api')
+  await fse.ensureDir(targetPath)
+  await fse.writeFile(path.join(targetPath, 'rule.json'), JSON.stringify(result))
+
+  const targetMediaPath = path.join(mainPath, 'build/media/rule')
+  await fse.ensureDir(targetMediaPath)
+  fse.copy(path.join(mainPath, 'rule/media'), targetMediaPath)
 }
 
 main().catch((e)=>{ console.error(e) })
