8568e4335dc97edeca5e30efeb68342ceb0f6cde
miaowm5 <miaowm5@gmail.com>
2020-08-19 20:04:54 +0800

    remove common ctx

--- src/common/ctx.js
+++
@@ -1,9 +0,0 @@
-
-import React, { useContext } from 'react'
-
-export default ()=>{
-  const Ctx = React.createContext(null)
-  const { Provider } = Ctx
-  const useCtx = ()=>useContext(Ctx)
-  return { useCtx, Provider }
-}
--- src/common/index.js
+++ src/common/index.js
@@ -1,9 +1,8 @@
 
 import getAPI from './getAPI'
 import useAPI from './useAPI'
-import ctx from './ctx'
 import router from './router'
 
 export {
-  getAPI, useAPI, ctx, router,
+  getAPI, useAPI, router,
 }
